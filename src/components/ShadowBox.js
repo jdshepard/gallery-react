26a65a5af2b073c4b465c69886c1e997d402b014
Jake McHargue <web@jakemchargue.com>
2018-03-19 14:24:39 -0500

    update shadowbox - add social buttons

--- src/components/ShadowBox.js
+++ src/components/ShadowBox.js
@@ -10,13 +10,39 @@ class ShadowBox extends Component {
     if (extension === 'mp4') {
       media = <video src={photoDatum.url} autoPlay loop alt="" />
     } else {
-      media = <img src={photoDatum.url} alt="" />
+      media = <div className="img" style={{backgroundImage: `url('${photoDatum.url}')`}}></div>
     }
     return (
       <div className="gallery-shadowBox">
         <Link to="/smilebooth-gallery-react"><div className="gallery-shadowBox-hittarget"></div></Link>
+        <div className="gallery-toolbox">
+          <ul className="gallery-toolbox-actions">
+            <li className="gallery-toolbox-action action-exit">
+              <div className="toolbox-action-iconHolder">
+                <i className="fas fa-times"></i>
+              </div>
+            </li>
+            <li className="gallery-toolbox-action">
+              <div className="toolbox-action-iconHolder">
+                <i className="fas fa-envelope"></i>
+              </div>
+            </li>
+            <li className="gallery-toolbox-action">
+              <div className="toolbox-action-iconHolder">
+                <i className="fas fa-comments"></i>
+              </div>
+            </li>
+          </ul>
+        </div>
         <div className="gallery-shadowBox-content">
-          {media}
+          <div className="gallery-shadowBox-loader loader"></div>
+          <div className="gallery-shadowBox-media">
+            {media}
+          </div>
+          <div className="gallery-shadowBox-navigation">
+            <div className="shadowBox-navigation shadowBox-navigationPrevious"><i className="fas fa-angle-left"></i></div>
+            <div className="shadowBox-navigation shadowBox-navigationNext"><i className="fas fa-angle-right"></i></div>
+          </div>
         </div>
       </div>
     )
