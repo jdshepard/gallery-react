78238046897e436f39500921ec39424045dc4325
Jake McHargue <web@jakemchargue.com>
2018-03-21 11:06:02 -0500

    shadowbox - start making close icon

--- src/components/ShadowBox.js
+++ src/components/ShadowBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import ShadowboxShare from './ShadowboxShare'
+import closeIcon from '../images/close-icon.svg'
 
 class ShadowBox extends Component {
 
