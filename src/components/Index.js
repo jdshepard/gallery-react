8941804eb2d3a3766b6e7ac7bff9762ee28d7447
Jake McHargue <web@jakemchargue.com>
2018-03-23 13:49:23 -0500

    gallery index - get unique galleries

--- src/components/Index.js
+++ src/components/Index.js
@@ -7,7 +7,8 @@ class Index extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      galleries: []
+      galleries: [],
+      searchInput: ''
     }
   }
 
@@ -15,24 +16,38 @@ class Index extends Component {
     const gallerListUrl = 'https://test-api.smilebooth.com/api/v4/folders/list-names-noauth'
     superagent.post(gallerListUrl).end((err, res) => {
       this.setState((prevState, props) => {
-        return {galleries: res.body}
+        return {galleries: this.uniqueGalleries(res.body)}
       })
     })
   }
 
-  render() {
+  updateSearch(e) {
+    this.setState({searchInput: e.target.value})
+  }
+
+  uniqueGalleries(galleries) {
     let galleryIdHash = {}
-    for (let i=0; i < this.state.galleries.length; i++) {
-      galleryIdHash[this.state.galleries[i].id] = this.state.galleries[i].name
+    for (let i=0; i < galleries.length; i++) {
+      galleryIdHash[galleries[i].id] = galleries[i].name
     }
     const galleryIds = Object.keys(galleryIdHash)
     const galleryNames = Object.values(galleryIdHash)
-    const galleriesDom = galleryIds.map((id, index) => {
-      return <li key={id}><Link to={`/smilebooth-gallery-react/gallery/${id}`}>{galleryNames[index]}</Link></li>
+    let uniqueGalleries = []
+    for(let i = 0; i < galleryIds.length; i++)
+      uniqueGalleries.push({id: galleryIds[i], name: galleryNames[i]})
+    return uniqueGalleries
+  }
+
+  render() {
+    const galleriesDom = this.state.galleries.map((gallery) => {
+      return <li key={gallery.id}><Link to={`/smilebooth-gallery-react/gallery/${gallery.id}`}>{gallery.name}</Link></li>
     })
     return (
       <div>
-        <h1>Galleries</h1>
+        <header>
+          <h1>Galleries</h1>
+          <input type="text" placeholder="search" value={this.state.searchInput} onChange={this.updateSearch.bind(this)} />
+        </header>
         <ul>
           {galleriesDom}
         </ul>
