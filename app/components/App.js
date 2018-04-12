01b1482db83bdc0db4c083410fc2c8aaf45de281
Jake McHargue <web@jakemchargue.com>
2018-04-12 10:55:02 -0500

    css - fix the loading

--- app/components/App.js
+++ app/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
-import '../styles/App.css'
 import Gallery from './Gallery'
 import Index from './Index'
 
--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './components/App'
+import './styles/App.css'
 
 ReactDOM.render((
   <Router><App /></Router>
