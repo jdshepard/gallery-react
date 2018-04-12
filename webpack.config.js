9f436509364184a9114e6b6474865a7c26339ea9
Jake McHargue <web@jakemchargue.com>
2018-04-12 10:08:22 -0500

    add URL loader for images

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,14 @@ module.exports = {
       {
         test: /.svg$/,
         loader: 'svg-loader'
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]'
+        }
       }
     ]
   }
