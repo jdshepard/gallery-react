100e947a748a22d0ebc96617e959c3b46c71c5b2
Jake McHargue <web@jakemchargue.com>
2018-04-12 13:17:26 -0500

    template - add fonts to HTML template

--- app/server/renderFullPage.js
+++ app/server/renderFullPage.js
@@ -2,7 +2,11 @@ export default function renderFullPage(html, preloadedState) {
   return `
     <!doctype html>
     <html>
-      <head><title>Smilebooth</title></head>
+      <head>
+        <title>Smilebooth</title>
+        <link rel="stylesheet" href="https://use.typekit.net/wpc4ivb.css">
+        <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+      </head>
       <body>
         <div id="root">${html}</div>
         <script>
