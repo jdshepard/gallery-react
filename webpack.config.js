2cc035baf822627d2447f670459cc0f6ad649918
Jake McHargue <web@jakemchargue.com>
2018-04-13 11:17:18 -0500

    split webpack into prod & dev settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,74 @@
-var path = require('path')
-var webpack = require('webpack')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const merge = require('webpack-merge')
 
-module.exports = [
-  {
-    devtool: 'inline-source-map',
-    entry: './app/index.js',
-    output: {
-      path: path.join(__dirname, 'build', 'public'),
-      filename: 'client.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /.js$/,
-          loader: 'babel-loader',
-          include: path.join(__dirname, 'app'),
-          exclude: /node_modules/,
-          query: {
-            presets: ['env', 'react']
-          }
-        },
-        {
-          test: /.css$/,
-          loader: ['style-loader', 'css-loader'],
-          include: path.join(__dirname, 'app', 'styles')
-        },
-        {
-          test: /.svg$/,
-          loader: 'svg-inline-loader'
-        },
-        {
-          test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            name: 'static/media/[name].[hash:8].[ext]'
-          }
+const devSettings = {
+  devtool: 'inline-source-map'
+}
+
+const commonModuleSettings = {
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        include: path.join(__dirname, 'app'),
+        exclude: /node_modules/,
+        query: {
+          presets: ['env', 'react']
         }
-      ]
-    }
-  },
-  // https://stackoverflow.com/a/37391247
-  {
-    devtool: 'inline-source-map',
-    entry: './app/server/index.js',
-    output: {
-      path: path.join(__dirname, 'build'),
-      filename: 'server.bundle.js',
-      libraryTarget: 'commonjs'
-    },
-    externals: [ /^(?!\.|\/).+/i, ],
-    target: 'node',
-    // https://github.com/webpack-contrib/css-loader/issues/447
-    node: {
-      fs: 'empty',
-      net: 'empty'
-    },
-    module: {
-      rules: [
-        {
-          test: /.js$/,
-          loader: 'babel-loader',
-          include: path.join(__dirname, 'app'),
-          exclude: /node_modules/,
-          query: {
-            presets: ['env', 'react']
-          }
-        },
-        {
-          test: /.css$/,
-          loader: ['style-loader', 'css-loader'],
-          include: path.join(__dirname, 'app', 'styles')
-        },
-        {
-          test: /.svg$/,
-          loader: 'svg-inline-loader'
-        },
-        {
-          test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            name: 'static/media/[name].[hash:8].[ext]'
-          }
+      },
+      {
+        test: /.css$/,
+        loader: ['style-loader', 'css-loader'],
+        include: path.join(__dirname, 'app', 'styles')
+      },
+      {
+        test: /.svg$/,
+        loader: 'svg-inline-loader'
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]'
         }
-      ]
-    }
+      }
+    ]
   }
-]
+}
+
+const clientConfig = {
+  entry: './app/index.js',
+  output: {
+    path: path.join(__dirname, 'build', 'public'),
+    filename: 'client.js'
+  }
+}
+
+const serverConfig = {
+  entry: './app/server/index.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'server.bundle.js',
+    libraryTarget: 'commonjs'
+  },
+  externals: [ /^(?!\.|\/).+/i, ],
+  target: 'node',
+  // https://github.com/webpack-contrib/css-loader/issues/447
+  node: {
+    fs: 'empty',
+    net: 'empty'
+  }
+}
+
+module.exports = mode => {
+  if (mode === "production") {
+    console.log('production settings')
+    return [merge(clientConfig, commonModuleSettings), merge(serverConfig, commonModuleSettings)]
+  }
+  console.log('dev settings')
+  return [merge(clientConfig, devSettings, commonModuleSettings), merge(serverConfig, devSettings, commonModuleSettings)]
+}
