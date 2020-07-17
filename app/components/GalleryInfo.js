be39e8495c242f6b0feaedc18ecaea22bee16d46
jdshepard <josh@smilebooth.com>
2020-07-17 12:06:31 -0500

    Update GalleryInfo.js
    update h2 to {gallery.name}

--- app/components/GalleryInfo.js
+++ app/components/GalleryInfo.js
@@ -4,7 +4,7 @@ class GalleryInfo extends Component {
   render() {
     return (
       <div className="gallery-info gallery-info-title">
-        <h2>Gallery Name Goes Here</h2>
+        <h2>{gallery.name}</h2>
       </div>
     )
   }
